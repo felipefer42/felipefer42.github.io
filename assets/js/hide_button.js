9a33bcf57355d3ddc44b2626bf22bb5e0058f70a
felipefer42 <felipefer42@gmail.com>
2023-10-23 17:16:40 -0300

    Updated version

---
+++ assets/js/hide_button.js
@@ -0,0 +1,32 @@
+function toggleJobDetails() {
+  var elements = document.querySelectorAll('.job-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleTeachDetails() {
+  var elements = document.querySelectorAll('.teach-details');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
+
+function toggleCareerSumamry() {
+  var elements = document.querySelectorAll('.summary-more');
+  elements.forEach(function(element) {
+    if (element.style.display === "none" || element.style.display === "") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+}
